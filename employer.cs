ba5cb4ce71e30a254b1daa6289100eeb60e2c786
Sándorné Feke Réka <sandornef@kkszki.hu>
2025-03-16 01:35:30 +0100

    Employer konstruktor kesz

--- employer.cs
+++ employer.cs
@@ -7,12 +7,22 @@ using System.Threading.Tasks;
 
 namespace Employee
 {
-    public class employer
+    public class Employer
     {
         public int Azonosito { get; set; }
         public string Nev { get; set; }
         public int Kor { get; set; }
         public int Kereset { get; set; }
 
+        public Employer(string row)
+        {
+            string[] darabol = row.Split(';');
+
+            Azonosito = int.Parse(darabol[0]);
+            Nev = darabol[1];
+            Kor = int.Parse(darabol[2]);
+            Kereset = int.Parse(darabol[3]);
+        }
     }
+
 }
