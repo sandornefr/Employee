61cebfc0ed89a0de5fd67e815639257a4a947834
Sándorné Feke Réka <sandornef@kkszki.hu>
2025-03-16 01:09:24 +0100

    Employer osztalyban modositas

--- employer.cs
+++ employer.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Employee
 {
-    class employer
+    public class employer
     {
+        public int Azonosito { get; set; }
+        public string Nev { get; set; }
+        public int Kor { get; set; }
+        public int Kereset { get; set; }
+
     }
 }
